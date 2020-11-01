b4d06e075ecffbb8df81d06edd088b6cf442eae0
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-01 12:49:40 +0300

    Task-3: Headers added

--- product-service/handlers/getProductById.js
+++ product-service/handlers/getProductById.js
@@ -12,17 +12,29 @@ export const getProductById = async (event) => {
         if (!foundProduct) {
             return {
                 statusCode: 404,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
                 body: JSON.stringify({ error: BOOK_NOT_FOUND })
             };
         }
 
         return {
             statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
             body: JSON.stringify(foundProduct)
         };
     } catch (error) {
         return {
             statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
             body: JSON.stringify({ error: SOMETHING_WENT_WRONG })
         };
     }
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -5,11 +5,19 @@ export const getProductsList = async () => {
     try {
         return {
             statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
             body: JSON.stringify(booksList)
         };
     } catch (error) {
         return {
             statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
             body: JSON.stringify({ error: SOMETHING_WENT_WRONG })
         };
     }
