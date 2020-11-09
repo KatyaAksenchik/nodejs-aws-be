058aedbfbee79141ffa38c995981b39456974f6e
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-09 20:53:01 +0300

    Task-4: Change to LF formating

--- product-service/constants/dbOptions.js
+++ product-service/constants/dbOptions.js
@@ -1,1 +1,19 @@
-const {    PG_HOST,    PG_PORT,    PG_DATABASE,    PG_USERNAME,    PG_PASSWORD,} = process.env;export const dbOptions = {    host: PG_HOST,    port: PG_PORT,    database: PG_DATABASE,    user: PG_USERNAME,    password: PG_PASSWORD,    ssl: {        rejectUnauthorized: false,    },    connectionTimeoutMillis: 5000,};
+const {
+    PG_HOST,
+    PG_PORT,
+    PG_DATABASE,
+    PG_USERNAME,
+    PG_PASSWORD,
+} = process.env;
+
+export const dbOptions = {
+    host: PG_HOST,
+    port: PG_PORT,
+    database: PG_DATABASE,
+    user: PG_USERNAME,
+    password: PG_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    connectionTimeoutMillis: 5000,
+};
--- product-service/handlers.js
+++ product-service/handlers.js
@@ -1,1 +1,11 @@
-import { getProductById } from './handlers/getProductById.js';import { getProductsList } from './handlers/getProductsList.js';import { postProduct } from './handlers/postProduct.js';import { invokeDb } from './handlers/pgClientLambda.js';export {    getProductById,    getProductsList,    postProduct,    invokeDb,}
+import { getProductById } from './handlers/getProductById.js';
+import { getProductsList } from './handlers/getProductsList.js';
+import { postProduct } from './handlers/postProduct.js';
+import { invokeDb } from './handlers/pgClientLambda.js';
+
+export {
+    getProductById,
+    getProductsList,
+    postProduct,
+    invokeDb,
+}
