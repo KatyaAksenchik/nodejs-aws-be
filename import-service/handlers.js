7b5ea8d40384a71d288591a9e0e291bf55f879dc
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-14 18:55:10 +0300

    Task-5: Import to s3

---
+++ import-service/handlers.js
@@ -0,0 +1,1 @@
+import { importProductsFile } from './handlers/importProductsFile.js';import { importFileParser } from './handlers/importFileParser.js';export {    importProductsFile,    importFileParser,}
