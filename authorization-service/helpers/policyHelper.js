7ac4c87b00a8c7e6d1ed69aae08415d126b069a7
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-30 09:20:53 +0300

    Task-7: LF

--- authorization-service/helpers/policyHelper.js
+++ authorization-service/helpers/policyHelper.js
@@ -1,1 +1,15 @@
-export const generatePolicy = (principalId, resource, effect = 'Allow') => {    return {        principalId,        policyDocument: {            Version: '2012-10-17',            Statement: [                {                    Action: 'execute-api:Invoke',                    Effect: effect,                    Resource: resource,                },            ],        },    };};
+export const generatePolicy = (principalId, resource, effect = 'Allow') => {
+    return {
+        principalId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: resource,
+                },
+            ],
+        },
+    };
+};
